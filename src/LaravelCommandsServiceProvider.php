867860497b6ac02fbcd06e616aaf41dda163d177
Noah Boos <noahboos-dev@proton.me>
2026-07-14 05:26:32 +0200

    Chore - Created and registered the package's service provider.

---
+++ src/LaravelCommandsServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace NoahBoos\LaravelCommands;
+
+use Illuminate\Support\ServiceProvider;
+use NoahBoos\LaravelCommands\Commands\MakeServiceCommand;
+
+class LaravelCommandsServiceProvider extends ServiceProvider {
+    /**
+     * Register services.
+     */
+    public function register(): void {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void {
+        //
+    }
+}
